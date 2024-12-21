1f5436a07dbdfee33ec433aa26f7f518311eaaa8
mageshkannan02 <mageshkannan022005@gmail.com>
2024-12-21 16:15:55 +0530

    feat(ui): :sparkles: components completed
    components slotcard->availabel slots and available dates ,infobox,naming tag

--- packages/ui/src/components/componentStyled/infobox/index.tsx
+++ packages/ui/src/components/componentStyled/infobox/index.tsx
@@ -11,6 +11,7 @@ interface InfoProps {
 
 export const InfoCard: React.FC<InfoProps> = ({ data }) => {
 	return (
+		// w-[38rem]  is taken 100% of the their width now i have given 38 rem due to i dont have parent elemet today and 38 is for sample
 		<Card className='m-3 w-[38rem] shadow-none border-0 flex  bg-[#F7F9FF] px-2'>
 			<CardBody className=' flex justify-between items-center flex-row h-[4.3rem]'>
 				<p className='text-[#555555] font-medium '>
