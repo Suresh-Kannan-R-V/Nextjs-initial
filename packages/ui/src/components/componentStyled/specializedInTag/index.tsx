8ae24c1f22b9cde1540a0060b6006035b1245b78
THIRUSELVAN-V <integratedthiruselvan4@gmail.com>
2024-12-23 15:50:44 +0530

    refactor(ui): :recycle: added interface

--- packages/ui/src/components/componentStyled/specializedInTag/index.tsx
+++ packages/ui/src/components/componentStyled/specializedInTag/index.tsx
@@ -1,8 +1,12 @@
 'use client';
+interface SpecializedInTagProps {
+	icon?: React.ReactNode;
+	specializedIn?: string;
+}
 export const SpecializedInTag = ({
 	icon,
 	specializedIn,
-}: { icon?: React.ReactNode; specializedIn?: string }) => {
+}: SpecializedInTagProps) => {
 	return (
 		<div className='w-[18.813rem] h-10 px-[0.625rem] flex items-center rounded-tl-borderRadius8px rounded-bl-borderRadius8px gap-1 font-regular text-body2  bg-gradient-to-r from-[#F8971F1F] to-[#F8F8F83D]'>
 			{icon}
