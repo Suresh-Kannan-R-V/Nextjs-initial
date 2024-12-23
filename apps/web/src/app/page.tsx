1bec5f745203186c992b98856efb6410214fb9ea
THIRUSELVAN-V <integratedthiruselvan4@gmail.com>
2024-12-23 14:07:17 +0530

    an error solved

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,7 +7,7 @@ import { Creditsimg } from '../../../../packages/ui/assets';
 export default function Page() {
 	return (
 		<div className='bg-slate-400 flex flex-col gap-0 justify-center align-middle h-screen mx-auto'>
-			<div className='flex justify-center align-middle'></div>
+			<div className='flex justify-center align-middle'>div</div>
 		</div>
 	);
 }
